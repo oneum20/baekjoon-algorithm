c528c2eec8fdd7d485cc33763cdb7ded737712c2
han2290 <woom2290@gmail.com>
2022-05-10 12:19:58 +0900

    EX 2309

---
+++ 정렬/EX2309.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class EX2309 {
+    static int[] nums = new int[9];
+    static int[] exn = new int[2];
+    static int sum;
+
+    static void except() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - nums[i] - nums[j] == 100) {
+                    exn[0] = nums[i];
+                    exn[1] = nums[j];
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < 9; i++) {
+            nums[i] = in.nextInt();
+            sum += nums[i];
+        }
+
+        except();
+
+        Arrays.sort(nums);
+        for (int i : nums) {
+            if (i != exn[0] && i != exn[1])
+                System.out.print(i + " ");
+        }
+
+    }
+
+}
